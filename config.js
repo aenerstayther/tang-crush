464b36a7b51466491106fa6d3cd14961126daf14
aenerstayther <106578540+aenerstayther@users.noreply.github.com>
2022-05-31 13:41:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'bro à!',
+    introDesc: `:)))`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    title: 'cũng sắp đến sinh nhật mày rồi á',
+    desc: 'mày muốn quà gì nà ',
+    btnYes: 'thôi không cần đâuu',
+    btnNo: 'cải gì cũng được'
+    question:'nay tốt ghê hem',
+    btnReply: 'gửi con ngan con',
+    reply: 'tao mà lại',
+    mess: 'tao biết mà , ew mày',
+    messDesc: 'chủ nhật t sẽ điii sinh nhật mày haaaaaa h paiiii.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
